8c26893e89ee6a52bb418865f8e734b75ef6cf9b
AmanSingh494 <130276810+AmanSingh494@users.noreply.github.com>
2023-08-12 15:27:30 +0000

    Updated app.js and navbar.js :improved night mode functionality.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 import About from './components/About'
+import React, {useState} from "react"
 function App() {
+  const [mode, setMode] = useState("dark")
+  const toggleMode = () => {
+    if (mode === "light") {
+       setMode("dark")
+    }else{
+setMode("light")
+}
+    
+  }
   return (
     <>
-      <Navbar title='TextUtils' />
+      <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode}/>
       <TextForm heading='Enter text here to analyse' />
       <About />
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <nav
-      className='navbar navbar-expand-lg navbar-dark bg-dark rounded'
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} rounded`}
       aria-label='Eleventh navbar example'
     >
       <div className='container-fluid'>
@@ -63,6 +63,10 @@ export default function Navbar(props) {
               aria-label='Search'
             />
           </form>
+          <div className="form-check form-switch">
+  <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
+  <label className="form-check-label text-light" for="flexSwitchCheckDefault">Enable dark mode</label>
+</div>
         </div>
       </div>
     </nav>
